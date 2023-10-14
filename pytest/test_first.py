69a038d83dfd5c3e0d635406c4261395b1d3b690
Trinity Tushabe <trinitytushabe@gmail.com>
2023-10-14 18:34:14 +0300

    Test First TDD Cycle 2 1. 2. Green:.

--- pytest/test_first.py
+++ pytest/test_first.py
@@ -1,9 +1,11 @@
 def multiply(a, b):
-    return 1
+    return  a*b
 
 
 def test_multiply():
-    result = multiply(1, 1)
-    assert result == 1
+    result1 = multiply(1, 1)
+    assert result1 == 1
 
-    assert result == 4
+    result2 = multiply(2, 2)
+    assert result2 == 4
+
